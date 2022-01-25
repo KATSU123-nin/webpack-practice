0f8f2a45bd4d604f58e5e3d7f5af6e783a238663
KATSU <66921645+KATSU123-nin@users.noreply.github.com>
2022-01-25 22:45:46 +0900

    Installed css-loader & style-loader and described the settings in the webpack.config.js file.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { message1 } from './modules/sub1'
 import { message2 } from './modules/sub2'
+import './styles/style.css';
 
 console.log('コチラは index.js の内容を表示しています。');
 
--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,25 @@ const path = require('path');
 
 module.exports = {
     mode: 'development',
-    entry: {main: './src/main.js'},
+    entry: { main: './src/main.js' },
+    devtool: false,
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader'
+                    }
+                ]
+            },
+        ]
     }
 };
